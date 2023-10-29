70990cec8a2337f8703f3107c5f7587dce913295
msm <myatsumon@ucsy.edu.mm>
2023-10-29 14:54:05 +0630

    sending request and response

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+// app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json());
 
 app.use("/feed", feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,21 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is the first post!",
+      },
+    ],
+  });
+};
+
+exports.postPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
