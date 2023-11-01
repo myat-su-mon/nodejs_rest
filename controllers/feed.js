814f2ccdb2e76fcd302bd689f013b0f13cf789da
msm <myatsumon@ucsy.edu.mm>
2023-11-01 21:36:57 +0630

    connecting posts and users

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator");
@@ -64,22 +65,29 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path.replace("\\", "/");
-  console.log(imageUrl);
-
-  // Create post in db
+  let creator;
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "mg mg" },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       console.log(result);
       res.status(201).json({
         message: "Post created successfully!",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
