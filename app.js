19b97d464b07b668dcbe2e4fd30c25ba4ef5965f
msm <myatsumon@ucsy.edu.mm>
2023-11-02 21:47:35 +0630

    setting up socket.io on server

--- app.js
+++ app.js
@@ -62,7 +62,11 @@ mongoose
   )
   .then((result) => {
     console.log("connected");
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("socket.io")(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => {
     console.log(err);
